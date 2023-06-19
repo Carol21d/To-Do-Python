4c4b2666494ae0d6327fb0beaba01c3ef0aec308
blake S <design.harls@icloud.com>
2023-06-19 17:15:50 +0200

    creando una clase todolist y una funcion init

---
+++ 01-py/To-Do.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+class ToDoList:
+    def _init_(self, root):
+        self.tasks = []
+        self.root = root
+        self.listbox = Listbox(self, root)
+        self.entry = Entry(self, root)
+        self.addButton = Button(
+            self, root, text="Add Task", command=self.add_task)
+        self.delButton = Button(
+            self, root, text="Delete Task", command=self.delete_task)
