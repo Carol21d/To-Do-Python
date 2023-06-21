81f39a7ca02c8c44d8d40aeb768586d97d35220c
blake S <design.harls@icloud.com>
2023-06-21 11:08:05 +0200

    creando btn con funcion al hacerle click

--- 02-py/To-Do-List-2.py
+++ 02-py/To-Do-List-2.py
@@ -3,15 +3,24 @@ from tkinter import *
 
 root = Tk()
 # create a label widget
-myLabel = Label(root, text="Hello World!!")
-myLabel2 = Label(root, text="Hola Mundo")
-
+# myLabel = Label(root, text="Hello World!!")
+# myLabel2 = Label(root, text="Hola Mundo")
 # showing it into the screen
 # myLabel.pack()
-
 # redisize las labels
-myLabel.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+# myLabel.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=0)
+
+
+# creamos una funcion
+def _click_btn():
+    mylabel = Label(root, text="Haz hecho click en el boton")
+    mylabel.pack()
+
+
+# creating buttons
+myBtn = Button(root, text="Click here", command=_click_btn)
+myBtn.pack()
 
 
 root.mainloop()
