91f73c818f52abe4fd4c096c1e2401790fd7e48f
blake S <design.harls@icloud.com>
2023-06-21 12:31:10 +0200

    calculadora con botones lista

--- 03-py/Calculadora.py
+++ 03-py/Calculadora.py
@@ -2,44 +2,55 @@ from tkinter import *
 
 
 root = Tk()
+root.title("Simple Calculator")
 
 # entrada de datos
-entrada = Entry(root, width=20)
-entrada.pack()
+entrada = Entry(root, width=35, borderwidth=5)
+entrada.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+
+# funcion
+
+def add_btn():
+    return
+
 
 # buttons
-btn_01 = Button(root, text="1")
-btn_01.pack()
-btn_02 = Button(root, text="2")
-btn_02.pack()
-btn_03 = Button(root, text="3")
-btn_03.pack()
-btn_04 = Button(root, text="4")
-btn_04.pack()
-btn_05 = Button(root, text="5")
-btn_05.pack()
-btn_06 = Button(root, text="6")
-btn_06.pack()
-btn_07 = Button(root, text="7")
-btn_07.pack()
-btn_08 = Button(root, text="8")
-btn_08.pack()
-btn_09 = Button(root, text="9")
-btn_09.pack()
-btn_00 = Button(root, text="0")
-btn_00.pack()
-btn_Equals = Button(root, text="=")
-btn_Equals.pack()
-btn_Clear = Button(root, text="Clear")
-btn_Clear.pack()
-btn_Subs = Button(root, text="+")
-btn_Subs.pack()
-btn_Rest = Button(root, text="-")
-btn_Rest.pack()
-btn_Multi = Button(root, text="x")
-btn_Multi.pack()
-btn_Div = Button(root, text="%")
-btn_Div.pack()
+btn_01 = Button(root, text="1", padx=40, pady=20, command=add_btn)
+btn_02 = Button(root, text="2", padx=40, pady=20, command=add_btn)
+btn_03 = Button(root, text="3", padx=40, pady=20, command=add_btn)
+btn_04 = Button(root, text="4", padx=40, pady=20, command=add_btn)
+btn_05 = Button(root, text="5", padx=40, pady=20, command=add_btn)
+btn_06 = Button(root, text="6", padx=40, pady=20, command=add_btn)
+btn_07 = Button(root, text="7", padx=40, pady=20, command=add_btn)
+btn_08 = Button(root, text="8", padx=40, pady=20, command=add_btn)
+btn_09 = Button(root, text="9", padx=40, pady=20, command=add_btn)
+btn_00 = Button(root, text="0", padx=40, pady=20, command=add_btn)
+btn_Equals = Button(root, text="=", padx=40, pady=20, command=add_btn)
+btn_Clear = Button(root, text="C", padx=40, pady=20, command=add_btn)
+btn_Subs = Button(root, text="+", padx=40, pady=20, command=add_btn)
+btn_Rest = Button(root, text="-", padx=40, pady=20, command=add_btn)
+btn_Multi = Button(root, text="x", padx=40, pady=20, command=add_btn)
+btn_Div = Button(root, text="%", padx=40, pady=20, command=add_btn)
+
+
+# ordenando los buttons en la pantalla
+btn_09.grid(row=1, column=2)
+btn_08.grid(row=1, column=1)
+btn_07.grid(row=1, column=0)
+btn_06.grid(row=2, column=2)
+btn_05.grid(row=2, column=1)
+btn_04.grid(row=2, column=0)
+btn_03.grid(row=3, column=2)
+btn_02.grid(row=3, column=1)
+btn_01.grid(row=3, column=0)
+btn_00.grid(row=4, column=1)
+btn_Equals.grid(row=4, column=0)
+btn_Clear.grid(row=4, column=2)
+btn_Subs.grid(row=1, column=3)
+btn_Rest.grid(row=2, column=3)
+btn_Multi.grid(row=3, column=3)
+btn_Div.grid(row=4, column=3)
 
 
 root.mainloop()
