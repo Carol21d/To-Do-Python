3bbf735a2fc432b77902124fe329e4f821538a96
blake S <design.harls@icloud.com>
2023-06-21 10:58:03 +0200

    grid

--- 02-py/To-Do-List-2.py
+++ 02-py/To-Do-List-2.py
@@ -4,8 +4,14 @@ from tkinter import *
 root = Tk()
 # create a label widget
 myLabel = Label(root, text="Hello World!!")
+myLabel2 = Label(root, text="Hola Mundo")
 
 # showing it into the screen
-myLabel.pack()
+# myLabel.pack()
+
+# redisize las labels
+myLabel.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
 
 root.mainloop()
