e57135e8265848d29cc28b1e43c6ba2ab6460bba
blake S <design.harls@icloud.com>
2023-06-24 15:18:39 +0200

    using images

--- 04-py/Using-Images.py
+++ 04-py/Using-Images.py
@@ -3,4 +3,9 @@ from tkinter import *
 
 root = Tk()
 root.title("Learn to Python")
-root.iconbitmap('')
+# root.iconbitmap('c:/gui/codemy.ico')
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
+
+root.mainloop()
