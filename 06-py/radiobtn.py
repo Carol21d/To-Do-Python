e8f2c7c36b486236de264603a90d12ce3c29fbf9
blake S <design.harls@icloud.com>
2023-06-29 18:54:25 +0200

    clicked button

--- 06-py/radiobtn.py
+++ 06-py/radiobtn.py
@@ -5,4 +5,21 @@ root = Tk()
 root.title('Learn Radio Button')
 
 
+r = IntVar()
+r.set(2)
+
+
+def clicked(value):
+    myLabel = Label(root, text=value)
+    myLabel.pack()
+
+
+Radiobutton(root, text="Option 1", variable=r, value=1,
+            command=lambda: clicked(r.get())).pack()
+Radiobutton(root, text="Option 2", variable=r, value=2,
+            command=lambda: clicked(r.get())).pack()
+
+
+myLabel = Label(root, text=r.get())
+myLabel.pack()
 mainloop()
