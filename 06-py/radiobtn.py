dd49c0b40b0a45dad7e2a83dda80a8cc4aa27fb8
blake S <design.harls@icloud.com>
2023-06-29 19:07:30 +0200

    button works

--- 06-py/radiobtn.py
+++ 06-py/radiobtn.py
@@ -5,8 +5,23 @@ root = Tk()
 root.title('Learn Radio Button')
 
 
-r = IntVar()
-r.set(2)
+# r = IntVar()
+# r.set(2)
+
+MODES = [
+    ("PINEAPPLE", "PINEAPPLE"),
+    ("APPLE", "APPLE"),
+    ("WATERMELON", "WATERMELON"),
+    ("WINE", "WINE"),
+]
+
+
+drinks = StringVar()
+drinks.set("PINEAPPLE")
+
+
+for text, mode in MODES:
+    Radiobutton(root, text=text, variable=drinks, value=mode).pack()
 
 
 def clicked(value):
@@ -14,12 +29,16 @@ def clicked(value):
     myLabel.pack()
 
 
-Radiobutton(root, text="Option 1", variable=r, value=1,
-            command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2,
-            command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 1", variable=r, value=1,
+#             command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 2", variable=r, value=2,
+#             command=lambda: clicked(r.get())).pack()
 
 
-myLabel = Label(root, text=r.get())
+myLabel = Label(root, text=drinks.get())
 myLabel.pack()
+
+
+mybtn = Button(root, text="Click me !", command=lambda: clicked(drinks.get()))
+mybtn.pack()
 mainloop()
