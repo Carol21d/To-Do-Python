20439a6a4c0c7b23ae4d342fabc44b3e1cd06aae
blake S <design.harls@icloud.com>
2023-06-29 18:34:44 +0200

    creado button

--- 05-py/frames.py
+++ 05-py/frames.py
@@ -6,7 +6,10 @@ root.title('Learn Frames')
 
 
 frame = LabelFrame(root, text=" This is my frame..", padx=5, pady=5)
-frame.pack(padx=10, pady=10)
+frame.pack(padx=100, pady=100)
 
+
+b = Button(frame, text="Dont click here ")
+b.pack()
 
 root.mainloop()
