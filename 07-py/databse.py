d9fc9ed2d5f7393d3842a9d62c38e9b050736ea4
blake S <design.harls@icloud.com>
2023-07-01 18:47:49 +0200

    data in the table

--- 07-py/databse.py
+++ 07-py/databse.py
@@ -7,13 +7,20 @@ root = Tk()
 root.title('Database')
 
 # Db
-conn = sqlite3.connect()
+conn = sqlite3.connect("address_book.db")
 
 # CURSOR
 c = conn.cursor()
 
 # Create table
-c.exectue()
+c.exectue("""CREATE TABLE addresses(
+        first_name text,
+        last_name text,
+        address text,
+        city text,
+        state text,
+        zipcode integer
+        )""")
 
 
 # Commit
