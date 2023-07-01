5dcd845003ee56461b9eeae92d46c37949ddeaff
blake S <design.harls@icloud.com>
2023-07-01 19:04:57 +0200

    fixing naming

--- 07-py/databse.py
+++ 07-py/databse.py
@@ -44,23 +44,23 @@ f_zipcode.grid(row=5, column=1, padx=20)
 
 
 # Create text box labels
-f_name_label = Label(root, text="First Name")
+f_name_label = Label(root, text="First Name: ")
 f_name_label.grid(row=0, column=0)
 
-l_name_label = Label(root, text="Last Name")
-l_name_label.grid(row=0, column=0)
+l_name_label = Label(root, text="Last Name: ")
+l_name_label.grid(row=1, column=0)
 
-f_address_label = Label(root, text="Your Address ")
-f_address_label .grid(row=0, column=0)
+f_address_label = Label(root, text="Address: ")
+f_address_label .grid(row=2, column=0)
 
-f_city_label = Label(root, text="Your City ")
-f_city_label.grid(row=0, column=0)
+f_city_label = Label(root, text="City: ")
+f_city_label.grid(row=3, column=0)
 
-f_state_label = Label(root, text="Your State")
-f_state_label.grid(row=0, column=0)
+f_state_label = Label(root, text="State: ")
+f_state_label.grid(row=4, column=0)
 
-f_zipcode_label = Label(root, text="Your Zipcode")
-f_zipcode_label.grid(row=0, column=0)
+f_zipcode_label = Label(root, text="Zipcode: ")
+f_zipcode_label.grid(row=5, column=0)
 # Commit
 conn.commit()
 
