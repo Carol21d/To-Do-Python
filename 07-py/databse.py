b4abeec981884b1b17fc5e73b6747ebbe27b846f
blake S <design.harls@icloud.com>
2023-07-01 18:54:25 +0200

    table data

--- 07-py/databse.py
+++ 07-py/databse.py
@@ -13,15 +13,34 @@ conn = sqlite3.connect("address_book.db")
 c = conn.cursor()
 
 # Create table
-c.exectue("""CREATE TABLE addresses(
-        first_name text,
-        last_name text,
-        address text,
-        city text,
-        state text,
-        zipcode integer
-        )""")
+# c.execute("""CREATE TABLE addresses(
+#         first_name text,
+#         last_name text,
+#         address text,
+#         city text,
+#         state text,
+#         zipcode integer
+#         )""")
 
+
+f_name = Entry(root, width=30)
+f_name.grid(row=0, column=0, padx=20)
+
+f_last = Entry(root, width=30)
+f_last.grid(row=0, column=0, padx=20)
+
+f_address = Entry(root, width=30)
+f_address.grid(row=0, column=0, padx=20)
+
+f_city = Entry(root, width=30)
+f_city.grid(row=0, column=0, padx=20)
+
+
+f_state = Entry(root, width=30)
+f_state.grid(row=0, column=0, padx=20)
+
+f_zipcode = Entry(root, width=30)
+f_zipcode.grid(row=0, column=0, padx=20)
 
 # Commit
 conn.commit()
